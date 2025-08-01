3c5655253ff37bdde3af1f3f8b44f3dda2f7bc5f
linelotus <189575054+linelotus@users.noreply.github.com>
2025-08-01 20:46:29 +0000

    tictactoe

--- src/js/components/Bpage.jsx
+++ src/js/components/Bpage.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 
+
 function Square({ value, onSquareClick }) {
   return (
     <button className="square" onClick={onSquareClick}>
@@ -32,8 +33,9 @@ function Board({ xIsNext, squares, onPlay }) {
 
   return (
     <>
+    <div className="gridview container">
       <div className="status">{status}</div>
-      <div className="board-row">
+      <div className="board-row  ">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
         <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
@@ -47,7 +49,7 @@ function Board({ xIsNext, squares, onPlay }) {
         <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
         <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
         <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-      </div>
+      </div></div>
     </>
   );
 }
--- src/js/components/Fpage.jsx
+++ src/js/components/Fpage.jsx
@@ -20,17 +20,20 @@ function PlayerSelection({ onStart }) {
 
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+    <div className="flex flex-col items-center justify-center min-h-screen btn btn-light">
+      <input type="text" id="player1" placeholder="Enter player 1 name"></input>
+      <input type="text" id="player2" placeholder="Enter player 2 name"></input>
       <h1 className="text-3xl font-bold mb-6">Choose X or O</h1>
      
-      <div className="flex gap-6">
+      <div className="flex ">
         <button
           className={`px-6 py-3 text-xl rounded-lg ${
             player1Choice === "X" ? "bg-blue-500 text-white" : "bg-gray-300"
           }`}
           onClick={() => handleChoice("X")}>  X
         </button>
-        
+
+       
         <button
           className={`px-6 py-3 text-xl rounded-lg ${
             player1Choice === "O" ? "bg-blue-500 text-white" : "bg-gray-300"
@@ -42,10 +45,10 @@ function PlayerSelection({ onStart }) {
 
 
       {player1Choice && (
-        <p className="mt-6 text-lg">
+        <div className="mt-6 text-lg">
           Player 1: <strong>{player1Choice}</strong> | Player 2:{" "}
           <strong>{player2Choice}</strong>
-        </p>
+        </div>
       )}
 
 
@@ -54,10 +57,7 @@ function PlayerSelection({ onStart }) {
         className={`mt-8 px-6 py-3 text-xl rounded-lg ${
           player1Choice ? "bg-green-500 text-white" : "bg-gray-400"
         }`}
-        onClick={handleStart}
-      >
-        Start Game
-      </button>
+        onClick={handleStart}> Start Game </button>
     </div>
   );
 }
